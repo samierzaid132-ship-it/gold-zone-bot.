368d2d884173740c87e1d20755f8351aeac7fb79
samierzaid132-ship-it <samierzaid132@gmail.com>
2026-09-22 00:02:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,20 @@
+import os
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
 
